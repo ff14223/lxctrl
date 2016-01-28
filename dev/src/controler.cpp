0a094a3f8613ae23def2233d68932f394eb006b0
Georg Lanzl <georg.lanzl@gmx.at>
2016-01-28 16:50:20 +0100

    Alarmhandling

--- dev/src/controler.cpp
+++ dev/src/controler.cpp
@@ -9,9 +9,5 @@ void ctrl_general(ISystemData *pSystemData, ISystemSignals *pSignals)
 
 void ctrl_alarm(ISystemData *pSystemData, ISystemSignals *pSignals)
 {
-    if( pSignals->alarm.pInRaiseAlarm->get() == true )
-    {
-        if( pSignals->alarm.pInRaiseHausalarm->raised() )
-            pSystemData->alarm.pHausalarm->raise();
-    }
+
 }
